3c5736c1acedd08005b79743e94143a6c62ec203
Reut212 <reuthadad123@gmzil.com>
2021-03-23 21:00:13 +0200

    init repo

---
+++ Board.hpp
@@ -0,0 +1,10 @@
+#include "Direction.hpp"
+
+namespace ariel {
+//post()
+    void post(unsigned int row, unsigned int column,Direction direction ,string message);
+//read()
+    string read(unsigned int row, unsigned int column,Direction direction ,unsigned int length);
+//show()
+    void show();
+}
